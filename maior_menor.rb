c6762a38d2984cd9da39679e4997b0752afbeba0
Breno <brenodias002@gmail.com>
2020-12-09 17:23:14 -0300

    teste

--- maior_menor.rb
+++ maior_menor.rb
@@ -2,3 +2,10 @@ puts "Bem vindo ao jogo de adivinhação"
 puts "Digite seu nome"
 nome = gets
 puts "Vamos começar o jogo, " + nome
+puts "Escolhendo um número secreto entre 0 e 200"
+numero_secreto = 175
+puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+puts "Tentativa 1"
+puts "Entre com o numero"
+chute = gets
+puts "Será que acertou? Você chutou " + chute
