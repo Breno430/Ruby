4ab15fb6a4e49c73b714958b9cb24d9e9ca8ab5b
Breno <brenodias002@gmail.com>
2020-12-14 18:59:32 -0300

    ajuste

--- Ruby3/fogefoge.rb
+++ Ruby3/fogefoge.rb
@@ -16,7 +16,26 @@ def encontra_jogador(mapa)
     end
 end
 
-
+def posicoes_validas_a_partir_de(mapa, posicao)
+  posicoes = []
+  baixo = [posicao[0] + 1, posicao[1]]
+  if posicao_valida? mapa, baixo
+    posicoes << baixo
+  end
+  cima = [posicao[0] - 1,  posicao[1]]
+  if posicao_valida? mapa, cima
+    posicoes << cima
+  end
+  direita = [posicao[0], posicao[1] + 1]
+  if posicao_valida? mapa, direita
+    posicoes << direita
+  end
+  esquerda = [posicao[0], posicao[1] - 1]
+  if posicao_valida? mapa, esquerda
+    posicoes << esquerda
+  end
+  posicoes
+end
 
 
 def calcula_nova_posicao(heroi, direcao)
@@ -56,27 +75,28 @@ def posicao_valida?(mapa, posicao)
 end
 
 
+def move_fantasma(mapa, linha, coluna)
+  posicoes = posicoes_validas_a_partir_de mapa,[linha,coluna]
+  if posicoes.empty?
+    return
+  end
+
+  posicao = posicoes[0]
+  mapa[linha][coluna] = " "
+  mapa[posicao[0]][posicao[1]] = "F"
+end
+
 def move_fantasmas(mapa)
   caracter_do_fantasma = "F"
   mapa.each_with_index do |linha_atual, linha|
     linha_atual.chars.each_with_index do |caractere_atual, coluna|
       eh_fantasma = caractere_atual == caracter_do_fantasma
       if eh_fantasma
-        move_fantasma mapa, linha, coluna
+        move_fantasma(mapa, linha, coluna)
       end
     end
   end
 end
-
-
-def move_fantasma(mapa, linha, coluna)
-  posicao = [linha, coluna + 1]
-  if posicao_valida? mapa, posicao
-    mapa[linha][coluna] = " "
-    mapa[posicao[0]][posicao[1]] = "F"
-  end
-end
-
 
 def joga(nome)
   mapa = le_mapa(2)
@@ -92,7 +112,7 @@ def joga(nome)
 
     mapa[heroi[0]][heroi[1]] = " "
     mapa[nova_posicao[0]][nova_posicao[1]] = "H"
-    move_fantasmas mapa
+    move_fantasmas(mapa)
 
   end
 end
