e392e6ad91d0cd6043acede7b470950ac30a78f5
Breno <brenodias002@gmail.com>
2020-12-15 15:58:11 -0300

    ajustes

--- Ruby3/fogefoge.rb
+++ Ruby3/fogefoge.rb
@@ -1,4 +1,5 @@
 require_relative 'ui'
+require_relative 'heroi'
 
 def le_mapa(numero)
   arquivo = "mapa#{numero}.txt"
@@ -11,45 +12,29 @@ def encontra_jogador(mapa)
   mapa.each_with_index do |linha_atual, linha|
     coluna_heroi = linha_atual.index caractere_do_heroi
         if coluna_heroi
-          return [linha, coluna_heroi]
+          jogador = Heroi.new
+          jogador.linha = linha
+          jogador.coluna = coluna_heroi
+          return jogador
         end
-    end
+  end
+  nil
 end
 
-def posicoes_validas_a_partir_de(mapa, posicao)
+def soma_vetor(vetor1, vetor2)
+  [vetor1[0] + vetor2[0], vetor1[1] + vetor2[1]]
+end
+
+def posicoes_validas_a_partir_de(mapa, novo_mapa, posicao)
   posicoes = []
-  baixo = [posicao[0] + 1, posicao[1]]
-  if posicao_valida? mapa, baixo
-    posicoes << baixo
-  end
-  cima = [posicao[0] - 1,  posicao[1]]
-  if posicao_valida? mapa, cima
-    posicoes << cima
-  end
-  direita = [posicao[0], posicao[1] + 1]
-  if posicao_valida? mapa, direita
-    posicoes << direita
-  end
-  esquerda = [posicao[0], posicao[1] - 1]
-  if posicao_valida? mapa, esquerda
-    posicoes << esquerda
+  movimentos = [[+1, 0], [0, +1], [-1, 0], [0, -1]]
+  movimentos.each do |movimento|
+    nova_posicao = soma_vetor(movimento, posicao)
+    if posicao_valida?(mapa, nova_posicao) && posicao_valida?(novo_mapa,nova_posicao)
+      posicoes << nova_posicao
+    end
   end
   posicoes
-end
-
-
-def calcula_nova_posicao(heroi, direcao)
-  heroi = heroi.dup
-  movimentos = {
-    "W" => [-1, 0],
-    "S" => [+1, 0],
-    "A" => [0, -1],
-    "D" => [0, +1]
-  }
-  movimento = movimentos[direcao]
-  heroi[0] += movimento[0]
-  heroi[1] += movimento[1]
-  heroi
 end
 
 def posicao_valida?(mapa, posicao)
@@ -75,45 +60,59 @@ def posicao_valida?(mapa, posicao)
 end
 
 
-def move_fantasma(mapa, linha, coluna)
-  posicoes = posicoes_validas_a_partir_de mapa,[linha,coluna]
+def move_fantasma(mapa, novo_mapa, linha, coluna)
+  posicoes = posicoes_validas_a_partir_de mapa, novo_mapa,[linha,coluna]
   if posicoes.empty?
     return
   end
 
-  posicao = posicoes[0]
+  aleatoria = rand posicoes.size
+  posicao = posicoes[aleatoria]
   mapa[linha][coluna] = " "
-  mapa[posicao[0]][posicao[1]] = "F"
+  novo_mapa[posicao[0]][posicao[1]] = "F"
+end
+
+def copia_mapa(mapa)
+  novo_mapa = mapa.join("\n").tr("F", " ").split "\n"
 end
 
 def move_fantasmas(mapa)
   caracter_do_fantasma = "F"
+  novo_mapa = copia_mapa mapa
   mapa.each_with_index do |linha_atual, linha|
     linha_atual.chars.each_with_index do |caractere_atual, coluna|
       eh_fantasma = caractere_atual == caracter_do_fantasma
       if eh_fantasma
-        move_fantasma(mapa, linha, coluna)
+        move_fantasma(mapa, novo_mapa, linha, coluna)
       end
     end
   end
+  novo_mapa
 end
+
+def jogador_perdeu?(mapa)
+  perdeu = !encontra_jogador(mapa)
+end
+
 
 def joga(nome)
   mapa = le_mapa(2)
   while true
     desenha mapa
     direcao = pede_movimento
-
     heroi = encontra_jogador mapa
-    nova_posicao = calcula_nova_posicao(heroi, direcao)
-    if !posicao_valida? mapa, nova_posicao
+    nova_posicao = heroi.calcula_nova_posicao(direcao)
+    if !posicao_valida? mapa, nova_posicao.to_array
       next
     end
+    heroi.remove_do(mapa)
+    nova_posicao.coloca_no(mapa)
 
-    mapa[heroi[0]][heroi[1]] = " "
-    mapa[nova_posicao[0]][nova_posicao[1]] = "H"
-    move_fantasmas(mapa)
-
+    mapa = move_fantasmas(mapa)
+    if jogador_perdeu? mapa
+      game_over
+      break
+    end
   end
 end
 
